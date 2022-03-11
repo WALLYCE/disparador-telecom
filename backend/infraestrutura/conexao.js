076321c202656e66b784a1d64fc4e1709ad36782
WALLYCE <wallyceazy@gmail.com>
2022-03-10 22:26:28 -0300

    Update conexao.js

--- backend/infraestrutura/conexao.js
+++ backend/infraestrutura/conexao.js
@@ -1,11 +1,11 @@
 const pg = require('pg');
 
 const config ={
-    host: '192.168.87.254',
-    user:  'eserv_leitura',
-    password: 'a01065c76fb3c612e2520b19aacf93e6',
-    database: 'hubsoft',
-    port:     '9432'
+    host: '',
+    user:  '',
+    password: '',
+    database: '',
+    port:     ''
 }
 const conexao = new pg.Client(config);
 
